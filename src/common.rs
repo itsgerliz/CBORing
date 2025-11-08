09eb75e7e6e2dd20667202828505ca1767521b5c
itsgerliz <itsgerliz@bonai.es>
2025-11-08 19:45:36 +0100

    ft(common): major type convenience constants

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,13 @@
+// Major types constants, used to avoid writing major types everywhere
+pub(crate) const UNSIGNED_INTEGER: u8   = 0b000_00000;
+pub(crate) const NEGATIVE_INTEGER: u8   = 0b001_00000;
+pub(crate) const BYTE_STRING: u8        = 0b010_00000;
+pub(crate) const TEXT_STRING: u8        = 0b011_00000;
+pub(crate) const ARRAY_OF_ITEMS: u8     = 0b100_00000;
+pub(crate) const MAP_OF_ITEMS: u8       = 0b101_00000;
+pub(crate) const TAGGED_ITEM: u8        = 0b110_00000;
+pub(crate) const FLOAT_SIMPLE_BREAK: u8 = 0b111_00000;
+
 enum JumpKind {
 	UnsignedInteger(u8),  // Major type 0
 	NegativeInteger(u8),  // Major type 1
